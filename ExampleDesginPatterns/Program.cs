5f638a0594d4b5aeddbabf8e1b1a42f65aece5f1
Sabrina <sabrina.o.cardoso@outlook.com>
2022-04-09 16:04:08 -0300

    preojeto iniciado

---
+++ ExampleDesginPatterns/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExampleDesginPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestSingleton();
+            TestfactoryMethod();
+            Console.ReadLine();
+        }
+
+
+        static void TestSingleton(){
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+
+            if (s1 == s2)
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+
+            else
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+        }
+
+        static void TestfactoryMethod()
+        {
+
+        }
+    }
+}
---
+++ ExampleDesginPatterns/Singleton/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleDesginPatterns
+{
+    //Objetivo: Manter apenas uma instancia da classe.
+    public sealed class Singleton
+    {
+        static Singleton _intance;
+
+        //Fazer o construtor padrão privado( proíbe qualquer acesso externo à própria classe, inclusive das classes filhas.)
+        //para prevenir que outros objetos usem o operador “new” com a classe singleton.
+        private Singleton() { }
+
+
+        //Metodo estático(não pode ser instanciado) que retorna a instancia do objeto.
+        public static Singleton GetInstance()
+        {
+            if (_intance == null)
+                return _intance = new Singleton();
+            else
+                return _intance;
+
+        }
+    }
+}
